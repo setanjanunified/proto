5ec8918a2f5d814f21879c2c0de5de5e4bbd289e
Setanjan Roy <setanjan@unifiedinfotech.net>
2020-12-18 18:11:14 +0530

    Updates

--- index.js
+++ index.js
@@ -19,12 +19,20 @@ app.get('/offers',(req,res)=>{
 })
 
 app.get('/deeplink/:id',(req,res)=>{
-    axios.get('https://arabclicks.api.hasoffers.com/Apiv3/json?api_key=80367cdcf1809a07a3e0b57ed948af3dea2334c4d22854916cd7398b537c726e&Target=Affiliate_Offer&Method=generateTrackingLink&offer_id='+req.params.id)
-    .then(response=>{
+    if(req.query.url){
+     axios.get('https://arabclicks.api.hasoffers.com/Apiv3/json?api_key=80367cdcf1809a07a3e0b57ed948af3dea2334c4d22854916cd7398b537c726e&Target=Affiliate_Offer&Method=generateTrackingLink&offer_id='+req.params.id+'&params[url]='+req.query.url)
+      .then(response=>{
+          res.send({data:response.data.response.data});
+       })
+       } else
+        {
+          axios.get('https://arabclicks.api.hasoffers.com/Apiv3/json?api_key=80367cdcf1809a07a3e0b57ed948af3dea2334c4d22854916cd7398b537c726e&Target=Affiliate_Offer&Method=generateTrackingLink&offer_id='+req.params.id)
+          .then(response=>{
          res.send({data:response.data.response.data});
-    })
+         })
+        }
 })
 
-app.listen(3000,()=>{
+app.listen(3013,()=>{
      console.log('running');
 })
